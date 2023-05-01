8606fe6d40ba1ed3145964fb5bdedbcf29c9352d
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2023-05-01 12:19:26 +0300

    Completed sort-the-ods kata

--- Python/sort_the_odd_nums.py
+++ Python/sort_the_odd_nums.py
@@ -1,3 +1,12 @@
 # You will be given an array of numbers. 
 # You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
+def sort_array(source_array):
+    for index in range(0, len(source_array)):
+        for digit in range(index, len(source_array)):
+            if source_array[index] % 2 != 0 and source_array[digit] % 2 != 0:
+                if source_array[digit] < source_array[index]:
+                    tmp = source_array[index]
+                    source_array[index] = source_array[digit]
+                    source_array[digit] = tmp
+    return source_array
